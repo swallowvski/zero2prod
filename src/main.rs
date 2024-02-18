c837bf3508fdf39964d77f7d631dc04d0e68b3c1
Kenichi Yanagi <ahoudori7779@icloud.com>
2024-02-18 09:37:25 +0900

    add health check and add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    run()?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,19 @@
+#[actix_web::test]
+async fn health_check_work() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request .");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to spwan our app.");
+    let _ = actix_web::rt::spawn(server);
+}
