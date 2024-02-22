a1a30d9001c5defd8101ff73380e97c062dcb4ef
Kenichi Yanagi <ahoudori7779@icloud.com>
2024-02-22 18:25:00 +0900

    chenge setting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
     run,
@@ -15,9 +14,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to tread configuration.");
-    let connection =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+    let connection = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
